570905cd157f12c325e66d06b42c2845d146f0ab
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-08-07 20:28:17 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,71 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({
-  publicRoutes: [
-    '/',
+// export default authMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/api/webhook',
+//     '/question/:id',
+//     '/tags',
+//     '/tags/:id',
+//     '/profile/:id',
+//     '/community',
+//     '/jobs'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook', '/api/chatgpt'
+//   ]
+// });
+ 
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+ 
+
+
+import { NextResponse } from 'next/server';
+import { authMiddleware } from '@clerk/nextjs';
+
+export default function middleware(req) {
+  const url = req.nextUrl.clone();
+
+  // Define the paths to bypass
+  const bypassPaths = [
     '/api/webhook',
+    '/api/success',
     '/question/:id',
     '/tags',
     '/tags/:id',
     '/profile/:id',
     '/community',
     '/jobs'
-  ],
-  ignoredRoutes: [
-    '/api/webhook', '/api/chatgpt'
-  ]
-});
- 
+  ];
+
+  // Check if the request path matches any bypass path
+  if (bypassPaths.some(path => url.pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  // Apply Clerk Auth middleware
+  return authMiddleware({
+    publicRoutes: [
+      '/',
+      '/api/webhook',
+      '/api/success',
+      '/question/:id',
+      '/tags',
+      '/tags/:id',
+      '/profile/:id',
+      '/community',
+      '/jobs'
+    ],
+    ignoredRoutes: [
+      '/api/webhook',
+      '/api/success',
+      '/api/chatgpt'
+    ]
+  })(req);
+}
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
